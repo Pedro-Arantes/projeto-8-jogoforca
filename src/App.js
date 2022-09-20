96aefaab00faa0d16ec431f72762384d69f9fbc6
Pedro-Arantes <developer9pedro@gmail.com>
2022-09-20 12:41:57 -0300

    adicionando css e jsx estático

---
+++ src/App.js
@@ -0,0 +1,53 @@
+
+
+export default function App() {
+
+
+    return (
+
+        <main>
+            <div class="boxForca">
+                <img  src="/assets/forca0.png" alt="nada"  />
+                    <div class="boxBtnEsc">
+                        <button>Escolher Palavra</button>
+                        <h1 class="tituloForca">
+                            r
+                            a
+                            d
+                            i
+                            o
+                            t
+                            i
+                            v
+                            i
+                            d
+                            a
+                            d
+                            e
+                        </h1>
+                    </div>
+            </div>
+            <div class="btnList">
+            <button>E</button>
+            <button>E</button>
+            <button>E</button>
+            <button>E</button>
+            <button>E</button>
+            <button>E</button>
+            <button>E</button>
+            <button>E</button>
+            <button>E</button>
+            <button>E</button>
+            <button>E</button>
+            <button>E</button>
+            <button>E</button>
+            <button>E</button>
+            </div>
+            <div class="boxChute">
+            <label for="">Já sei a palavra!</label>
+            <input type="text"></input>
+            <button>Chutar</button>
+            </div>
+        </main>
+    )
+}
